7b6c30bf7d413136072d5027c803af12f2d18996
JacobD10 <jrd089@uowmail.edu.au>
2017-05-13 14:00:01 +1000

    Hourly auto-commit at 14:00:01 on 13/05/2017

--- temporary_script.m
+++ temporary_script.m
@@ -1,55 +1,9 @@
-f=1000;
-c=343;
-k = 2*pi*f/c;
-
-fmid=1000;
-kset=2*pi*fmid/c;
-
-d=0.02; %kd<1
-
-nT=5;
-x = linspace(-c/f*nT,c/f*nT,500);
-x1 = x-d/2;
-x2 = x+d/2;
-[xx,yy]=meshgrid(x);
-xx1=meshgrid(x1,x);
-xx2=meshgrid(x2,x);
-[tt,rr]=cart2pol(xx,yy);
-[tt1,rr1]=cart2pol(xx1,yy);
-[tt2,rr2]=cart2pol(xx2,yy);
-
-% r = abs(x);
-% r2 = abs(x2);
-
-
-a1 = besselh(0,k*rr);
-
-% compshift = exp(1i*pi*(1/2-2*d*f/c));
-% b1 = besselh(0,k*rr) * compshift;
-% c1 = besselh(0,k*rr2)*exp(1i*(1+2*d*f/c)*pi) * compshift;
-
-b1 = besselh(0,k*rr1) * exp( -1i*pi/2) * exp( 1i*pi*2*d*f/c);
-c1 = besselh(0,k*rr2) * exp( 1i*pi/2) ;
-
-b1 = besselh(0,k*rr1)  * (-1i/(k*d*2)) * exp( 1i*d/2*k);
-c1 = besselh(0,k*rr2)  * (1i/(k*d*2)) * exp( -1i*d/2*k);
-
-
-c1 = besselh(0,k*rr2)  * exp( 1i*(k*d - pi)/2) / (2*k*d);
-b1 = besselh(0,k*rr1)  * exp( 1i*(pi - k*d)/2) / (2*k*d);
-
-
-figure(11)
-surf( real(a1) ,'linestyle','none');view(2)
-figure(1)
-surf( real(b1+c1) ,'linestyle','none');view(2)
-
-figure(2)
-plot(x,real(a1(end/2,:)),'k'); hold on;
-% plot(x,real(-a1(end/2,:)),':k'); hold on;
-plot(x,real(b1(end/2,:)),'--r');hold on;
-plot(x,real(c1(end/2,:)),'--m');hold on;
-plot(x,real(b1(end/2,:)+c1(end/2,:)),'b');
-grid on; grid minor;
-hold off
-axis([-c/f*nT c/f*nT -2 2])
+Falias = Broadband_Tools.getAliasingFrequency(SYS.Main_Setup)/2/pi*SYS.signal_info.c / 1e3;
+SimCircBright=Res_Matrix{1}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
+SimCircQuiet=Res_Matrix{2}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
+RealCircQuiet=Res_Matrix{4}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
+RealCircBright=Res_Matrix{3}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
+mean(RealCircQuiet)
+mean(SimCircQuiet)
+mean(RealCircBright*2-RealCircQuiet*2)
+mean(SimCircBright*2-SimCircQuiet*2)
