673737527735b279fd480e8af531165823cd3884
JacobD10 <jrd089@uowmail.edu.au>
2017-06-10 00:00:02 +1000

    Hourly auto-commit at 00:00:02 on 10/06/2017

--- +Papers/+Oculus_Presentation/AR_Prediction.m
+++ +Papers/+Oculus_Presentation/AR_Prediction.m
@@ -5,7 +5,7 @@ SpeechFilePath = '\+Miscellaneous\+Speech_Files\Female_SA2.WAV';
 
 sig_info.Nfft = 12 * 1e-3 * sig_info.Fs; % Number of fft components
 sig_info.time_delay = []; % Seconds %If empty the time delay will based on the frame length
-sig_info.overlap = 1-1/2^5;
+sig_info.overlap = 1-1/2;
 sig_info.zeropadtime = 0; % miliseconds of zero padding (i.e. maximum time shift before circular convolution)
 sig_info.predict_buff = 2; % Length of prediction buffer as a percentage of a full frame prediction (i.e. 10 is %1000 of frame length)
 sig_info.AR_method = 'burg';
@@ -22,15 +22,17 @@ sig_info.window = false;
     Broadband_Tools.FFT_custom(x, sig_info);
 
 %%
-for EgTime = (sig_info.Nfft/sig_info.Fs)*3:((1-sig_info.overlap)*sig_info.Nfft/sig_info.Fs):(numel(x)/sig_info.Fs); % seconds
+% for EgTime = (sig_info.Nfft/sig_info.Fs)*3:((1-sig_info.overlap)*sig_info.Nfft/sig_info.Fs):(numel(x)/sig_info.Fs); % seconds
 % EgTime = 0.27; % seconds
 % EgTime = 0.61; % seconds
 % EgTime = 0.90; % seconds
+EgTime = 1.24; % seconds
 
 
 EgFrame = floor(EgTime * sig_info.Fs / ((1-sig_info.overlap) * sig_info.Nfft));
 N=1/(1-sig_info.overlap);
-%% Plot Prediction
+% Plot Prediction
+fH = figure(1);
 linewidth = 1.5;
 
 t = ((1:sig_info.Nfft*3) - sig_info.Nfft*2)/sig_info.Fs*1e3;
@@ -39,21 +41,46 @@ ax = gca;
 hold on
 plot(t(sig_info.Nfft*2+(1:sig_info.Nfft)), Frames_orig(EgFrame,:),':k','linewidth',linewidth);
 
-plot(t(sig_info.Nfft*2+(0:sig_info.Nfft)),[Frames_orig(EgFrame-N,end) Frames((EgFrame-N)*2,:)],'r','linewidth',linewidth);
+plot(t(sig_info.Nfft*2+(0:sig_info.Nfft)),[Frames_orig(EgFrame-N,end) Frames((EgFrame-N)*2,:)],'b','linewidth',linewidth);
+
+residual = Frames_orig(EgFrame,:)-Frames((EgFrame-N)*2,:);
+plot(t(sig_info.Nfft*2+(1:sig_info.Nfft)),residual,'r','linewidth',linewidth);
 
 % ylim(ax.YLim);
 ylim([min(x) max(x)]);
 xlim(t([1 end]));
 
-plot(t(sig_info.Nfft*2*[1 1]),ax.YLim,'-k','linewidth',linewidth);
+plot(t(sig_info.Nfft*2*[1 1]),ax.YLim,'-k','linewidth',linewidth*2);
 xx = (sig_info.Nfft*(1-sig_info.overlap) + [0 sig_info.Nfft*(sig_info.overlap)*[1 1] 0])/sig_info.Fs*1e3;
 yy = ax.YLim([1 1 2 2]);
-patch(xx,yy,[0 0 0],'edgecolor','none','facealpha',0.1)
+patch(xx,yy,[0 0 0],'edgecolor','none','facealpha',0.25)
 hold off
 
 ylabel('Amplitude')
 xlabel('Time (ms)')
+FontSz = 12;
+txtoffset = 1.1;% factor
+text(-1*sig_info.Nfft/sig_info.Fs*1e3,max(x)*txtoffset,...
+    'Past (Known Signal)',...
+    'FontSize',FontSz,'ho','c')
 
-drawnow;
-pause(1/30);
-end
+text(0.5*sig_info.Nfft/sig_info.Fs*1e3,max(x)*txtoffset,...
+    'Future (Predicted Signal)',...
+    'FontSize',FontSz,'ho','c','co','b')
+
+annotation('textarrow',...
+    ([(0.0+(txtoffset-1)/2) 0.25]*sig_info.Nfft/sig_info.Fs*1e3  - ax.XLim(1))/diff(ax.XLim) + ax.Position(1),...
+    ([min(x)*(1-(txtoffset-1)*2)...
+    residual(0.25*sig_info.Nfft)] - ax.YLim(1))/diff(ax.YLim) + ax.Position(2),...
+    'String','Residual',...
+    'FontSize',FontSz,'co','r')
+
+
+% drawnow;
+% pause(1/30);
+% end
+
+ax.FontSize = FontSz;
+fH.Color = 'w';
+% drawnow;
+% tightfig;
