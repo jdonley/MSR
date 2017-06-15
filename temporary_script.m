d869de5e4daecea44baa074160c11dd2c0792c28
JacobD10 <jrd089@uowmail.edu.au>
2017-06-15 23:00:02 +1000

    Hourly auto-commit at 23:00:02 on 15/06/2017

--- temporary_script.m
+++ temporary_script.m
@@ -1,10 +1,2 @@
-Falias = Broadband_Tools.getAliasingFrequency(SYS.Main_Setup)/2/pi*SYS.signal_info.c / 1e3;
-Falias = min(Falias,1.5938);
-SimCircBright=Res_Matrix{1}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
-SimCircQuiet=Res_Matrix{2}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
-RealCircQuiet=Res_Matrix{4}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
-RealCircBright=Res_Matrix{3}(Hrz_Vec>0.15 & Hrz_Vec<Falias);
-mean(RealCircQuiet)
-mean(SimCircQuiet)
-mean(RealCircBright*2-RealCircQuiet*2)
-mean(SimCircBright*2-SimCircQuiet*2)
+
+
