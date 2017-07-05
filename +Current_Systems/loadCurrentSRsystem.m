7ee5306d0897e8bd49e1fd35496618545977ea49
JacobD10 <jrd089@uowmail.edu.au>
2017-07-05 14:00:06 +1000

    Hourly auto-commit at 14:00:06 on 05/07/2017

--- +Current_Systems/loadCurrentSRsystem.m
+++ +Current_Systems/loadCurrentSRsystem.m
@@ -12,8 +12,8 @@ System_Path = Settings.DefaultSystemsPath;
 
 % Current_System = 'IEEETransactions_System_A'; % Differing Source Angles
 % Current_System = 'IEEETransactions_System_B'; % Differing Loudspeaker Count
-Current_System = 'IEEETransactions_System_C'; % Simulated and Real-World
-% Current_System = 'IEEETransactions_System_D'; % Differing Lambda-Grave Parameter
+% Current_System = 'IEEETransactions_System_C'; % Simulated and Real-World
+Current_System = 'IEEETransactions_System_D'; % Differing Lambda-Grave Parameter (All Angles)
 % Current_System = 'IEEETransactions_System_E'; % Acoustic Brightness Contrast - Simulated and Real-World
 
 % Current_System = 'NOVECOM_System';
