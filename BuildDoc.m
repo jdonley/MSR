5c6e0140816e105450d56080214a15d4e980826d
JacobD10 <jrd089@uowmail.edu.au>
2017-07-09 17:00:05 +1000

    Hourly auto-commit at 17:00:05 on 09/07/2017

--- BuildDoc.m
+++ BuildDoc.m
@@ -5,5 +5,5 @@ docdir = 'doc';
 %mainFile = '+Current_Systems\Evaluate_Current_System.m';
 %Tools.buildDocumentation( wrkdir, docdir, mainFile );
 
-mainFile = '+Current_Systems\loadCurrentSRsystem.m';
+mainFile = '+Current_Systems\loadCurrentSRsystemSCRIPT.m';
 Tools.buildDocumentation( wrkdir, docdir, mainFile, true );
