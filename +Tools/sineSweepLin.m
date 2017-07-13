4de6662077c555ff9e433d07d65b3a11e678eb06
JacobD10 <jrd089@uowmail.edu.au>
2017-07-13 19:15:57 +1000

    removed tools for submodule merge

--- +Tools/sineSweepLin.m
+++
@@ -1,8 +0,0 @@
-function [ y ] = sineSweepLin( f_start, f_end, dur, fs )
-%SINESWEEP_LIN Synthesize a linear sine sweep
-%   Detailed explanation goes here
-t=0:1/fs:dur-1/fs;
-f=linspace(f_start,(f_end+f_start)/2,length(t));
-y=sin(2*pi*f.*t);
-end
-
