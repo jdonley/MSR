3ddc7b227970b8193b511cf6f71ead0de51781ac
Jacob Ryan Donley <jrd089@uow.edu.au>
2017-07-29 20:00:02 +1000

    Hourly auto-commit at 20:00:01 on 29/07/2017

--- +Current_Systems/IEEETransactions_System_B.m
+++ +Current_Systems/IEEETransactions_System_B.m
@@ -262,7 +262,7 @@ publication_info.axes_gridMinorY = 'off'; % Override minor grid for Y axis ('on'
 publication_info.axes_gridMinorX = 'off'; % Override minor grid for X axis ('on' or 'off')
 publication_info.axes_tickdir = 'both'; % Axes tick direction(s) ('in', 'out' or 'both')
 publication_info.axes_NumTicks = [5 9]; % Number of ticks [NumXticks NumYticks];
-publication_info.axes_limitBufs = [0.02 0.02]; % axis limits buffer in percentage [width, height]
+publication_info.axes_limitBufs = [0.05 0.05]; % axis limits buffer in percentage [width, height]
 publication_info.sigRounding = 3; % number of significant figures rounding
 
 publication_info.FontSize = 9;  % Font size of text in figure
