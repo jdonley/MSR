688cc9c3b976a5932e337a2084aaa542d4bc9e20
Jacob Ryan Donley <jrd089@uow.edu.au>
2017-08-01 22:00:01 +1000

    Hourly auto-commit at 22:00:01 on 01/08/2017

--- +Room_Acoustics/Reverberant_MSR_Analysis.m
+++ +Room_Acoustics/Reverberant_MSR_Analysis.m
@@ -20,10 +20,13 @@ for rt = 1:Nrt
     for c_ = 1:numel(SYS.signal_info.methods_list_clean)
         c = SYS.signal_info.methods_list_clean(c_);
         if isfield(SYS.signal_info,'recording_type_list') ... <- This retains backwards compatibility
-                && strcmpi(...
+                && ~strcmpi(...
                     SYS.signal_info.recording_type_list(c), ...
-                    SYS.signal_info.recording_type)
-                
+                    SYS.signal_info.recording_type) ...
+                && paired
+                % If the paired setups are not of the correct recording 
+                % type then skip the analysis
+                continue; 
         end
         masker_list = SYS.signal_info.methods_list_masker;
         if paired
@@ -44,6 +47,20 @@ for rt = 1:Nrt
                 subSYS.signal_info.methods_list_masker = numel(c)+1:numel(c)+numel(m);
             else
                 subSYS.signal_info.methods_list_clean = 1;
+            end
+            if isfield(SYS.system_info,'CurrentSpeakerArrayType') ... <- This retains backwards compatibility
+                    && ...
+                    ~strcmpi( SYS.system_info.CurrentSpeakerArrayType, ...
+                    subSYS.Main_Setup.Speaker_Array_Type) ...
+                    && (SYS.signal_info.UseMeasuredATFs ...
+                    || strcmpi(SYS.signal_info.recording_type,'real-world'))
+                % If the objects array type is not the current physically
+                % set up type in the real-world and we are using measured
+                % ATFs (which use that current physical setup) or the 
+                % recording type is 'real-world' (which then must require a 
+                % different physical setup) then we skip the analysis to
+                % save time.
+                continue; 
             end
             
             Room_Acoustics.Apply_RIRs.Reverberant_MSR_Analysis_batchfunc( subSYS );
