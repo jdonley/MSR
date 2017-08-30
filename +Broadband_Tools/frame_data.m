49b587d0f6f07147aa4b052d1f708e65c5e40a08
JacobD10 <jrd089@uowmail.edu.au>
2017-08-30 16:00:02 +1000

    Hourly auto-commit at 16:00:02 on 30/08/2017

--- +Broadband_Tools/frame_data.m
+++ +Broadband_Tools/frame_data.m
@@ -29,25 +29,29 @@ function [ d_framed ] = frame_data( d, OL, N )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Find lengths of the overlapping and non-overlapping sections
-no_OL_sz = int64(N*(1-OL));
-OL_sz = N - no_OL_sz;
+hopSz = double(int64(N*(1-OL)));    % Hop size
+olSz = N - hopSz;                   % Overlap size
 
 % Pad the signal file to allow the creation of the matrix
-r = rem( length(d), no_OL_sz);
-r(r==0)=no_OL_sz;
-p = zeros( no_OL_sz - r , 1 );
-d = [d; p].';
-L = length(d) / no_OL_sz;
+L = ceil(length(d) / hopSz);              % Number of frames
+r = rem( length(d), hopSz);
+r(r==0)=hopSz;
+p = zeros( olSz - r, 1 );           % Zero padding
+d = [d; p].';                       % Extend length of input signal
+Lx = (length(d) + hopSz) / hopSz;             % L + int64(1/(1-OL))-1 % Extended number of frames
 
 % Find the overlapping section and non-overlapping section and concatenate
-partFrames = reshape(d, [no_OL_sz L]).';
-d_framed = partFrames; 
-for seg = 1:(int64(1/(1-OL))-1)
-    d_framed = [d_framed [partFrames((seg+1):end, 1:min(OL_sz,no_OL_sz)); zeros(seg,min(OL_sz,no_OL_sz))]];
-end
+d_framed = reshape(d(1:end-olSz), [hopSz L]).';
+OLinds = repmat((hopSz+1):N,L,1) ...
+    + repmat((0:hopSz:hopSz*(L-1)).',1,olSz);
+d_framed = [d_framed d(OLinds)];
+% d_framed = partFrames; 
+% for seg = 1:
+%     d_framed = [d_framed [partFrames((seg+1):end, 1:min(OL_sz,no_OL_sz)); zeros(seg,min(OL_sz,no_OL_sz))]];
+% end
 
 % Remove last frame if it contains no extra information other than an overlap
-if nnz(d_framed(end,:)) <= OL_sz
+if nnz(d_framed(end,:)) <= olSz
     d_framed = d_framed(1:(end-1),:);
     L = size(d_framed,1);
 end
