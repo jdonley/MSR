80692d9e22c96a8d4e3b3a80b54435786030dd51
JacobD10 <jrd089@uowmail.edu.au>
2017-09-14 00:00:03 +1000

    Hourly auto-commit at 00:00:03 on 14/09/2017

--- +Broadband_Tools/+Loudspeaker_Signal_Calculation/getPredictedLoudspeakerSignals.m
+++ +Broadband_Tools/+Loudspeaker_Signal_Calculation/getPredictedLoudspeakerSignals.m
@@ -145,6 +145,6 @@ if strcmpi(lSYS.Main_Setup.Speaker_Array_Type(1),'2')
          db2mag(-240)+0*flip(Loudspeaker_SignalsDP(1:end-(numel(hs)-1),:),2)] ;
 end
 
- Loudspeaker_Signals(:,(1:25)~=13) = db2mag(-240); %  TODO: remove this line, it is for debugging
+%  Loudspeaker_Signals(:,(1:25)~=13) = db2mag(-240); %  TODO: remove this line, it is for debugging
 
 end
