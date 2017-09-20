5256a4cd78604ccc8dfe8d1dde884441c094ae12
JacobD10 <jrd089@uowmail.edu.au>
2017-09-20 11:00:10 +1000

    Hourly auto-commit at 11:00:09 on 20/09/2017

--- temporary_script.m
+++ temporary_script.m
@@ -19,11 +19,15 @@ rtxN = 24;
 rtx = [zeros(rtxN*rtxN,1), yy(:), zz(:)];
 srx = rtx;
 
-tic
-for ss = 1:10
+MM=[];PP=[];
+tic;
+ss=0;
+while true %for ss = 1:10
+    ss = ss+1;
 % r = [1.0 1.5 1.5];    % Receiver positions [x_1 y_1 z_1 ; x_2 y_2 z_2] (m)
 r = rand(1,3)*3;    % Receiver positions [x_1 y_1 z_1 ; x_2 y_2 z_2] (m)
-s = [1.5 1.5 1.5];              % Source position [x y z] (m)
+% s = [1.5 1.5 1.5];              % Source position [x y z] (m)
+s = rand(1,3)*3;    % Receiver positions [x_1 y_1 z_1 ; x_2 y_2 z_2] (m)
 
 hA = rir_generator(c, fs, r, s, L, betaA, n, mtype, order, dim, orientation, hp_filter);
 h1 = rir_generator(c, fs, r, s, L, beta1, n, mtype, order, dim, orientation, hp_filter);
@@ -32,7 +36,6 @@ hf = h1(:)-hA(:);
 stx = s;              % Source position [x y z] (m)
 htx = rir_generator(c, fs, rtx, stx, L, betaA, n, mtype, order, dim, orientation, hp_filter);
 rrx = r;    % Receiver positions [x_1 y_1 z_1 ; x_2 y_2 z_2] (m)
-              % Source position [x y z] (m)
 for i = 1:size(rtx,1)
     hrx(i,:) = rir_generator(c, fs, rrx, srx(i,:), L, betaA, n, mtype, order, dim, orientation, hp_filter);
 end
@@ -62,22 +65,28 @@ MagnitudeC(end/2+1:end)=[];
 PhaseDifference = mod(unwrap(angle(HF)) - unwrap(angle(HC)+pi),2*pi)/pi*180-180;
 PhaseDifference(end/2+1:end)=[];
 
-MM(:,ss) = MagnitudeC.*ff.';
+MM(:,ss) = MagnitudeC; %.*ff.';
 PP(:,ss) = PhaseDifference;
-
+% MM = mean([MM , MagnitudeC.*ff.' ],2);
+% PP = mean([PP , PhaseDifference  ],2);
 
 
 subplot(2,1,1);
-plot(ff, mag2db(  MM  ));
+plot(ff, mag2db(  MM  ),':k'); hold on;
+plot(ff, mag2db(  mean(MM,2)  ),'-b','linew',1.5); hold off;
 xlim([0.1 10]); %ylim([-60 0]);
 grid on; grid minor; set(gca,'xscale','log');
+xlabel('Frequency (kHz)');ylabel('Magnitude (dB)');
 
 subplot(2,1,2);
-plot(ff, PP ,'.');
+plot(ff, PP ,':k'); hold on;
+plot(ff, mean(PP,2) ,'-','co',.8*[1 0 0],'linew',1.5); hold off;
 xlim([0.1 10]); ylim([-180 180]); yticks([-180:45:180]);
 grid on; grid minor; set(gca,'xscale','log');
+xlabel('Frequency (kHz)');ylabel('Phase (\circ)');
 
 drawnow;
+fprintf('%d\n',ss);
 end
 toc
 
