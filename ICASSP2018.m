351583ccd0d153882e767568e8a61e345a40c9ae
JacobD10 <jrd089@uowmail.edu.au>
2017-10-22 17:00:08 +1100

    Hourly auto-commit at 17:00:07 on 22/10/2017

--- ICASSP2018.m
+++ ICASSP2018.m
@@ -579,13 +579,60 @@ hh(:,:,:,3) = reshape(hh3,3*res,3*res,[]);
 
 [~,IC] = max(abs(hh(ceil(size(XX,1)/2),ceil(size(XX,2)/2),:,1))); % spatial-centre time-index
 
+hhRender = hh(:,2:end,:,:);
+maxV = max( abs( hhRender(:) ) ) * 0.5;
+C = repmat(linspace(0,1,256)',1,3);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure(100);
+FIELDREFLECTIONS = hh(:,:,IC, 1 );
+image((FIELDREFLECTIONS / maxV /1 + 1 ) * size(C,1)/2);
+title('Inactive');
+ax = gca; ax.Color(4) = 0;
+ax.YDir = 'normal'; ax.TickDir = 'both';
+axis([1 size(hh,1) 1 size(hh,2)]);
+ax.YTick = linspace(1,size(hh,1),4);
+ax.YTickLabel = linspace(0,3,4);
+ax.XTick = linspace(1,size(hh,2),4);
+ax.XTickLabel = linspace(0,3,4);
+ax.XLabel.String = 'Width (m)';
+ax.YLabel.String = 'Length (m)';
+set(gcf,'Position',[100 100 500 500])
+colormap(C);
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(111);
 FIELDERROR_method1 = diff(hh(:,:,IC,[1 2]),[],4);
-imagesc(FIELDERROR_method1);
+image((FIELDERROR_method1 / maxV /1 + 1 ) * size(C,1)/2);
+title('Active - Proposed WFS WLS');
+ax = gca; ax.Color(4) = 0;
+ax.YDir = 'normal'; ax.TickDir = 'both';
+axis([1 size(hh,1) 1 size(hh,2)]);
+ax.YTick = linspace(1,size(hh,1),4);
+ax.YTickLabel = linspace(0,3,4);
+ax.XTick = linspace(1,size(hh,2),4);
+ax.XTickLabel = linspace(0,3,4);
+ax.XLabel.String = 'Width (m)';
+ax.YLabel.String = 'Length (m)';
+set(gcf,'Position',[100 100 500 500])
+colormap(C);
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(222);
 FIELDERROR_method2 = diff(hh(:,:,IC,[1 3]),[],4);
-imagesc(FIELDERROR_method2);
+image((FIELDERROR_method2 / maxV /1 + 1 ) * size(C,1)/2);
+title('Active - Proposed FOD');
+ax = gca; ax.Color(4) = 0;
+ax.YDir = 'normal'; ax.TickDir = 'both';
+axis([1 size(hh,1) 1 size(hh,2)]);
+ax.YTick = linspace(1,size(hh,1),4);
+ax.YTickLabel = linspace(0,3,4);
+ax.XTick = linspace(1,size(hh,2),4);
+ax.XTickLabel = linspace(0,3,4);
+ax.XLabel.String = 'Width (m)';
+ax.YLabel.String = 'Length (m)';
+set(gcf,'Position',[100 100 500 500])
+colormap(C);
 
 %%
 % v = VideoWriter('IRcancelwall.avi','Uncompressed AVI');
