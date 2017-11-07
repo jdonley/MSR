41fdd1b8a391ea1972988ffc9267995227abf5d6
JacobD10 <jrd089@uowmail.edu.au>
2017-11-07 17:00:02 +1100

    Hourly auto-commit at 17:00:01 on 07/11/2017

--- +Papers/+Transactions_2016/SoundFieldVisualisations.m
+++ +Papers/+Transactions_2016/SoundFieldVisualisations.m
@@ -1,49 +1,19 @@
-%clc;
+clc;
 clear;
-%close all;
+close all;
 tic;
 
 %%
 % SYS = Current_Systems.loadCurrentSRsystem;
-
 SYS = Current_Systems.IEEETransactions_System_G;
 
 %%
-f = 1000;
-%  f = Broadband_Tools.getAliasingFrequency(SYS.Main_Setup(2))*343/2/pi;
-c = 343;
-% Freqs = 200:100:3000;
-C=[];E=[];
-%  for f = Freqs
-% fprintf('%.0f\n',f);
-%%
 setup = [SYS.Main_Setup(:);SYS.Masker_Setup(:)];
-
-for s = 1:numel(setup)
-    
-%         setup(s).Multizone_Soundfield.Radius = 0.91;
-%     setup(s).Multizone_Soundfield.UnattendedZ_Weight = 0;
-    
-%     setup(s).Multizone_Soundfield.Quiet_Zone = ...
-%         setup(s).Multizone_Soundfield.Quiet_Zone.setDesiredSoundfield(true, f, 'suppress_output');
-%     setup(s).Multizone_Soundfield.Bright_Zone = ...
-%         setup(s).Multizone_Soundfield.Bright_Zone.setDesiredSoundfield(true, f, 'suppress_output');
-%     setup(s).Multizone_Soundfield = setup(s).Multizone_Soundfield.setN( -1 ); %Auto set
-%     setup(s).Multizone_Soundfield = setup(s).Multizone_Soundfield.createEmptySoundfield('DEBUG');
-%     if setup(s).Loudspeaker_Count>1
-        setup(s).Multizone_Soundfield = setup(s).Multizone_Soundfield.createSoundfield('DEBUG');
-%     end
-    
-    setup(s) = setup(s).calc_Loudspeaker_Weights();
-%     if s == 2
-%        setup(s).Radius = (setup(s-1).Loudspeaker_Dimensions(1)*setup(s-1).Loudspeaker_Count/2)/2;
-% 
-%        setup(s).Multizone_Soundfield.Radius = setup(s).Radius;
-%    end
-    setup(s) = setup(s).reproduceSoundfield('DEBUG');
-    
+for s = 1:numel(setup)        
+    setup(s).Multizone_Soundfield = setup(s).Multizone_Soundfield.createSoundfield('DEBUG');    
+    setup(s) = setup(s).calc_Loudspeaker_Weights();    
+    setup(s) = setup(s).reproduceSoundfield('DEBUG');    
 end
-
 
 %%
 if exist('fH'), if isvalid(fH), close(fH); end; end
@@ -82,7 +52,7 @@ ha = tightPlots( ...
     dimSz(1), ...
     SYS.publication_info.figure_width, ...
     SYS.publication_info.axis_aspect_ratio, ...
-    [0.5 -0.4], ...
+    SYS.publication_info.axes_gap, ...
     SYS.publication_info.axes_margins_height, ...
     SYS.publication_info.axes_margins_width, ...
     'centimeters');
@@ -95,10 +65,15 @@ axes(ha(s));
 ax=gca;
 setup(s).plotSoundfield( mag2db(abs(F{s})), 'scientific_L12', realistic, details);
 
-text(10,size(F{s},1)-FontSize-10,1e3,['(' char(64+s) ')'],...
-    'BackgroundColor',[1 1 1 0.7], ...
-    'FontName',FontName,'FontSize',FontSize)
-ax.Title.String = '';
+text(0,size(F{s},1),1e3,['(' char(64+s) ')'],...
+    ... 'BackgroundColor',[1 1 1 0.7], ...
+    'FontName',FontName,'FontSize',FontSize, ...
+    'Interpreter','latex', ...
+    'HorizontalAlignment','left', 'VerticalAlignment','bottom');
+ax.Title = [];
+
+ax.XLabel.Interpreter = SYS.publication_info.Interpreter;
+ax.YLabel.Interpreter = SYS.publication_info.Interpreter;
 
 [c,r] = ind2sub(dimSz,s);
 if c ~= 1
@@ -118,15 +93,22 @@ colorbar off;
 end
 
 % 
- hCB = colorbar; hCB.Units = 'points';
- hCB.Ticks = interp1(1:length(caxis),caxis,linspace(1,length(caxis),5));
- hCB.TickLabels = num2str(linspace( ax.CLim(1), ax.CLim(2),5)' );
- hCB.Label.String = 'Magnitude (dB)';
+drawnow;
+hCB = colorbar; 
+hCB.Location = 'manual';
+hCB.Units = 'points';
+hCB.Ticks = interp1(1:length(caxis),caxis,linspace(1,length(caxis),5));
+hCB.TickLabels = num2str(linspace( ax.CLim(1), ax.CLim(2),5)' );
+hCB.Label.String = 'Magnitude (dB)';
 
- 
- tightfig;
- 
- % 
+ax.Units = 'centimeters';
+axWH = ax.Position(3:4);
+hCB.Units = 'centimeters';
+hCB.Position(4) = axWH(2) + 0.5 + axWH(2); % Colorbar height
+
+tightfig;
+
+%
 % set(fH.Children, 'Units','Points')
 % for c = 1:numel(fH.Children)
 %  fH.Children(c).Position(2) = fH.Children(c).Position(2)+20;
@@ -158,8 +140,8 @@ end
 
 
 
-C(end+1) = pow2db(setup(1).Acoustic_Contrast);
-E(end+1) = mag2db(setup(1).MSE_Bright);
+% C(end+1) = pow2db(setup(1).Acoustic_Contrast);
+% E(end+1) = mag2db(setup(1).MSE_Bright);
 
 %%
 disp(['   Contrast: ' num2str(pow2db(setup(1).Acoustic_Contrast)) 'dB']);
