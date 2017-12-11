efbf0a28a84cd5e5fd92368b2cf128472a36fc53
JacobD10 <jrd089@uowmail.edu.au>
2017-12-11 17:00:04 +1100

    Hourly auto-commit at 17:00:03 on 11/12/2017

--- +Papers/+ChinaSIP/Figure3.m
+++ +Papers/+ChinaSIP/Figure3.m
@@ -1,18 +1,22 @@
 %%
-load('Z:\+Soundfield_Database\+3m_SpkrDia\+16Spkrs_180DegArc_original\LUT_Weight_vs_Frequency_15deg_256f_128w.mat')
+% load('Z:\+Soundfield_Database\+3m_SpkrDia\+16Spkrs_180DegArc_original\LUT_Weight_vs_Frequency_15deg_256f_128w.mat')
 
 %%
+fh = figure(54321);
+close(fh);
+
 plot_width = 88.9/10;% + 6.35/10 + 88.9/10; %IEEE full text width (cm)
 
+fh = figure(54321);
 axH = tightPlots(2, 1, ...
     plot_width, ...
     [1 1/3], ...
-    [1 1], ...
+    [0.5 0.5], ...
     [1 1], ...
     [1 1], ...
     'centimeters');
 
-axes(axH(1)); ax = gca;
+axes(axH(1)); ax = gca; ax.Units = 'centimeters';
 surf(Frequencies/1e3,...
     mag2db(Weights),...
     mag2db(abs(Bright_Sample__Weight_Vs_Frequency)),...
@@ -20,10 +24,19 @@ surf(Frequencies/1e3,...
 view(2);
 set(gca,'xscale','log')
 colormap(gray);
-colorbar('location','eastoutside');
 caxis([-50 -15])
 xlim([0.15 8]);
+ylim([-40 80]);
+ax.TickDir = 'both';
 ax.XTick = [0.15 1 8];
+ax.XTickLabel = {};
+cbH = colorbar;
+cbH.Units = ax.Units;
+cbH.Location = 'manual';
+cbH.Position = [0.25 + sum(ax.Position([1 3])), ...
+                ax.Position(2), ...
+                0.5, ...
+                ax.Position(4)];
 
 
 axes(axH(2)); ax = gca;
@@ -38,6 +51,8 @@ colormap(gray);
 colorbar('location','eastoutside');
 caxis([-50 -15])
 xlim([0.15 8]);
+ylim([-40 80]);
+ax.TickDir = 'both';
 ax.XTick = [0.15 1 8];
 
 xlabel('Frequency (kHz)');
